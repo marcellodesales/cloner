7647e42c06b3617f4422885531181ea0fbf2ca6d
Marcello de Sales <Marcello_deSales@intuit.com>
2019-11-18 17:48:21 -0800

    Cloner:viper: setting up the util with Viper
    - Configuring how configs will be loaded for the CLI.
    - Using mapstructure to convert files with maps to struct easily.
    - Setting up viper to load configs from the home dir

--- cmd/root.go
+++ cmd/root.go
@@ -16,73 +16,72 @@ limitations under the License.
 package cmd
 
 import (
-  "fmt"
-  "github.com/sirupsen/logrus"
-  "github.com/spf13/cobra"
-  "github.com/marcellodesales/cloner/config"
-  "github.com/marcellodesales/cloner/util"
-  "os"
+	"fmt"
+	"github.com/marcellodesales/cloner/config"
+	"github.com/marcellodesales/cloner/util"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"os"
 )
 
 var cfgFile string
+
 //The verbose flag value
 var v string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-  Use:   "cloner",
-  Short: "Managing github clones with a single command",
-  Long: `cloner knows how to clone version-control urls by simply making sure
+	Use:   "cloner",
+	Short: "Managing github clones with a single command",
+	Long: `cloner knows how to clone version-control urls by simply making sure
 it can place software in specific location designed.`,
-  // Uncomment the following line if your bare application
-  // has an action associated with it:
-  //	Run: func(cmd *cobra.Command, args []string) { },
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-  if err := rootCmd.Execute(); err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 
 func init() {
-  // Here you will define your flags and configuration settings.
-  // Cobra supports persistent flags, which, if defined here,
-  // will be global for your application.
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
 
-  // Setup Viper Configuration file type
-  rootCmd.PersistentFlags().StringVar(&cfgFile, "cloner", "cloner", "Spec file (default is $HOME/cloner.yaml)")
+	// Setup Viper Configuration file type
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "cloner", "cloner", "Spec file (default is $HOME/cloner.yaml)")
 
-  // Callbacks to initilize, in order, for cobra-viper-anything else
-  cobra.OnInitialize(
-    func() {
-      // Sets up Viper with the given config file name
-      util.SetupViper(cfgFile)
-    },
-    func() {
-      // Setups up the configuration parsed from Viper config (maps -> structs)
-      config.Setup()
-    },
-  )
+	// Callbacks to initilize, in order, for cobra-viper-anything else
+	cobra.OnInitialize(
+		func() {
+			// Sets up Viper with the given config file name
+			util.SetupViper(cfgFile, "cloner", "CLONER")
+		},
+		func() {
+			// Setups up the configuration parsed from Viper config (maps -> structs)
+			config.Setup()
+		},
+	)
 
+	// Setup Logger https://le-gall.bzh/post/go/integrating-logrus-with-cobra/
+	rootCmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
+		if err := util.SetUpLogs(os.Stdout, v); err != nil {
+			return err
+		}
+		return nil
+	}
 
-  // Setup Logger https://le-gall.bzh/post/go/integrating-logrus-with-cobra/
-  rootCmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
-    if err := util.SetUpLogs(os.Stdout, v); err != nil {
-      return err
-    }
-    return nil
-  }
+	//Here is where we bind the verbose flag
+	//Default value is the info level
+	rootCmd.PersistentFlags().StringVarP(&v, "verbosity", "v", logrus.InfoLevel.String(), "Log level (debug, info, warn, error, fatal, panic")
 
-  //Here is where we bind the verbose flag
-  //Default value is the info level
-  rootCmd.PersistentFlags().StringVarP(&v, "verbosity", "v", logrus.InfoLevel.String(), "Log level (debug, info, warn, error, fatal, panic")
-
-  // Cobra also supports local flags, which will only run
-  // when this action is called directly.
-  //rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
---
+++ util/viper.go
@@ -0,0 +1,122 @@
+package util
+
+import (
+	"fmt"
+	"github.com/mitchellh/go-homedir"
+	"github.com/mitchellh/mapstructure"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"github.com/thoas/go-funk"
+	"os"
+	"strings"
+)
+
+func SetupViper(configFile, configName, envPrefix string) {
+	// https://github.com/spf13/viper/issues/390#issuecomment-511177615
+	viper.SetConfigType("yaml") // or viper.SetConfigType("YAML")
+	viper.AddConfigPath(".")
+
+	if configFile != "" {
+		viper.SetConfigName(configFile)
+
+	} else {
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		viper.AddConfigPath(home)
+		// the name of the config file
+		viper.SetConfigName(configName)
+	}
+
+	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
+	// the prefix of env vars to override the configs
+	viper.SetEnvPrefix(envPrefix)
+	viper.AutomaticEnv()
+}
+
+/**
+ * The path to the file used for configuration
+ */
+func GetConfigFilePath() string {
+	return viper.ConfigFileUsed()
+}
+
+/**
+ * Read the configuration and update the reference of a provided struct represented by the given config path
+ */
+func ReadConfig(configPath string, result interface{}) error {
+	configFilePath, err := getConfigFilePath()
+	if err != nil {
+		os.Exit(1)
+	}
+	log.Debugf("Loading the config object '%s' config from '%s'", configPath, configFilePath)
+
+	// Looking for the key of configuration in the config
+	log.Tracef("The keys in the config: %v", viper.AllKeys())
+	containsKey := false
+	for _, fullKey := range viper.AllKeys() {
+		if funk.Contains(fullKey, configPath) {
+			containsKey = true
+			break
+		}
+	}
+
+	// Verify if the key exists in the
+	if !containsKey {
+		log.Debugf("The config file does NOT contain the key '%s'", configPath)
+		return nil
+	}
+
+	// Load the MAP from viper config, just have the map
+	configMap := viper.Get(configPath)
+	log.Tracef("The loaded map is %v", configMap)
+
+	if err := fromMapToStruct(configPath, configMap, result); err != nil {
+		return err
+	}
+	return nil
+}
+
+/**
+ * Convert the map loaded from a config to a Struct provided
+ * https://stackoverflow.com/questions/26744873/converting-map-to-struct/26746461
+ */
+func fromMapToStruct(configPath string, configMap interface{}, configStruct interface{}) error {
+	// Use https://godoc.org/github.com/mitchellh/mapstructure to read properties from tagged structs
+	decoderConfig := &mapstructure.DecoderConfig{
+		WeaklyTypedInput: true,
+		Result:           &configStruct,
+	}
+
+	decoder, err := mapstructure.NewDecoder(decoderConfig)
+	if err != nil {
+		return err
+	}
+
+	err = decoder.Decode(configMap)
+	if err != nil {
+		log.Errorf("An error occurred while decoding key %s: %v", configPath, err)
+		return err
+	}
+
+	log.Tracef("Successfully transformed the map to struct: %v", configStruct)
+	return nil
+}
+
+func getConfigFilePath() (string, error) {
+	err := viper.ReadInConfig()
+	if err == nil {
+		// viper.ConfigFileUsed() only returns properties after it is read (filePathConfigured)
+		configFile := viper.ConfigFileUsed()
+
+		// viper.ConfigFileUsed() only returns properties after it is read
+		log.Debug("Using config file: ", configFile)
+		return configFile, nil
+
+	} else {
+		log.Errorf("Can't parse configuration: %s", err)
+		return "", err
+	}
+}
