4810c1b8440ccc83a9a44d1b0fc4f2e7d1aa2735
Marcello de Sales <marcello.desales@gmail.com>
2020-09-11 04:27:23 -0300

    :art: Isolate CLI implementation to be testable
    The current code couldn't be tested because of the basic implementation using cobra.
    This commit refacts the implementation to isolate the interface struct so that
    we can have a better reuse of the output of the commands. This is based on the
    implementation of the Docker CLI structure described at the following:
    
    https://stackoverflow.com/questions/59709345/how-to-implement-unit-tests-for-cli-commands-in-go/59714127#59714127

--- api/git/git_service.go
+++ api/git/git_service.go
@@ -35,59 +35,65 @@ var GitService GitServiceType
 // The regex to use to parse the repo https://play.golang.org/p/iBeytKqRDBX
 var githubRepoUrlRe = regexp.MustCompile(`(?m)^(?P<protocol>https|git)(:\/\/|@)(?P<host>[^\/:]+)[\/:](?P<user>[^\/:]+)\/(?P<repo>.+).git$`)
 
+// Init from the CLI inputs
+func (service GitServiceType) Init(repoUrl string, forceClone bool) *CloneGitRepoRequest {
+	return &CloneGitRepoRequest{
+		Url:   repoUrl,
+		Force: forceClone,
+	}
+}
+
 /**
  * @return a new instance of the GitRepoType
  */
-func (service GitServiceType) ParseRepoString(repo string) (*GitRepoClone, error) {
-	if repo == "" {
-		return nil, errors.New("you must provide the repo URL")
+func (service GitServiceType) ParseRepoString(gitRepoClone *CloneGitRepoRequest) error {
+	if gitRepoClone.Url == "" {
+		return errors.New("you must provide the repo URL")
 	}
 
-	if !strings.HasSuffix(repo, ".git") {
-		repo += ".git"
+	if !strings.HasSuffix(gitRepoClone.Url, ".git") {
+		gitRepoClone.Url += ".git"
 	}
 	// Parse the regex
-	gitRepoValues, err := util.RegexProcessString(githubRepoUrlRe, repo)
+	gitRepoValues, err := util.RegexProcessString(githubRepoUrlRe, gitRepoClone.Url)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	gitRepoInstance := GitRepoType{
+	gitRepoClone.Type = &GitRepoType{
 		Protocol: gitRepoValues["protocol"],
 		Host:     gitRepoValues["host"],
 		User:     gitRepoValues["user"],
 		Repo:     gitRepoValues["repo"], // may contain "/" when in repo is in gitlab folders format
 	}
 
-	gitRepoClone := &GitRepoClone{
-		Url:  repo,
-		Type: gitRepoInstance,
-	}
-
-	return gitRepoClone, nil
+	return nil
 }
 
 /**
  * Get the org
  */
-func (service GitServiceType) GetRepoLocalPath(gitRepoClone *GitRepoClone, config *config.Configuration) string {
+func (service GitServiceType) GetRepoLocalPath(gitRepoClone *CloneGitRepoRequest, config *config.Configuration) string {
 	return path.Join(config.Git.CloneBaseDir, gitRepoClone.Type.GetRepoDir())
 }
 
 /**
  * Get the repo clone dir expected to be created
  */
-func (service GitServiceType) GetOrgLocalPath(gitRepoClone *GitRepoClone, config *config.Configuration) string {
+func (service GitServiceType) GetOrgLocalPath(gitRepoClone *CloneGitRepoRequest, config *config.Configuration) string {
 	return path.Join(config.Git.CloneBaseDir, gitRepoClone.Type.GetUserDir())
 }
 
-func (service GitServiceType) VerifyCloneDir(gitRepoClone *GitRepoClone, forceClone bool, config *config.Configuration) (bool, error) {
+/**
+ * Make sure the clone dir does not exist. If it does, force clone must be provided to not fail.
+ */
+func (service GitServiceType) VerifyCloneDir(gitRepoClone *CloneGitRepoRequest, config *config.Configuration) (bool, error) {
 	// The location is provided by the api
 	gitRepoClone.CloneLocation = service.GetRepoLocalPath(gitRepoClone, config)
 	//log.Debugf("Verifying if the clone path '%s' exists or exists and is empty", gitRepoClone.CloneLocation)
 
 	if util.DirExists(gitRepoClone.CloneLocation) {
-		if forceClone {
+		if gitRepoClone.Force {
 			err := util.DeleteDir(gitRepoClone.CloneLocation)
 			if err != nil {
 				return false, fmt.Errorf("can't force deletion of dir '%s': %v", gitRepoClone.CloneLocation, err)
@@ -107,7 +113,7 @@ func (service GitServiceType) VerifyCloneDir(gitRepoClone *GitRepoClone, forceCl
 /**
  * Makes the base clone dir is the org and parts of the repo dir since the clone service creates the repo dir
  */
-func (service GitServiceType) MakeCloneDir(gitRepoClone *GitRepoClone, config *config.Configuration) error {
+func (service GitServiceType) MakeCloneDir(gitRepoClone *CloneGitRepoRequest, config *config.Configuration) error {
 	baseCloneDir := service.GetOrgLocalPath(gitRepoClone, config)
 
 	// For gitlab and other hosts with more than a single folder
@@ -130,8 +136,8 @@ func (service GitServiceType) MakeCloneDir(gitRepoClone *GitRepoClone, config *c
 /**
  * Clone the git repo to the clone location using go-git
  */
-func (service GitServiceType) GoCloneRepo(gitRepoClone *GitRepoClone, config *config.Configuration) error {
-	gitRepoClone.CloneLocation = service.GetRepoLocalPath(gitRepoClone, config)
+func (service GitServiceType) GoCloneRepo(gitRepoClone *CloneGitRepoRequest, config *config.Configuration) error {
+	//gitRepoClone.CloneLocation = service.GetRepoLocalPath(gitRepoClone, config)
 
 	// https://git-scm.com/book/en/v2/Appendix-B%3A-Embedding-Git-in-your-Applications-go-git
 	_, err := git.PlainClone(gitRepoClone.CloneLocation, false, &git.CloneOptions{
@@ -144,7 +150,7 @@ func (service GitServiceType) GoCloneRepo(gitRepoClone *GitRepoClone, config *co
 /**
  * Print the list of the files in the dir just like the "tree" unix command
  */
-func (service GitServiceType) GoPrintTree(gitRepoClone *GitRepoClone) (string, error) {
+func (service GitServiceType) GoPrintTree(gitRepoClone *CloneGitRepoRequest) (string, error) {
 	// Exclude the .git dir from the list
 	excludedList := []string{".git"}
 	return util.GetDirTree(gitRepoClone.CloneLocation, excludedList)
