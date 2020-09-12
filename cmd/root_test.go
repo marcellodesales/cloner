13187de27a4657584df8dda718a8f61f46d93d82
Marcello de Sales <marcello.desales@gmail.com>
2020-09-12 01:05:11 -0300

    :art: Improve tests: create default strategy
    Creating a single strategy for the tests will make it simpler to test:
    Just creating a template for the tests with the struct as an interface
    
    Based on https://github.com/elwinar/rambler/blob/master/apply_test.go

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -16,12 +16,14 @@ limitations under the License.
 package cmd
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
 	"github.com/marcellodesales/cloner/config"
 	"github.com/marcellodesales/cloner/util"
 	"github.com/sirupsen/logrus"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 /**
@@ -43,5 +45,59 @@ func setup() {
 
 	if err := util.SetUpLogs(os.Stdout, logrus.InfoLevel.String()); err != nil {
 		os.Exit(1)
+	}
+}
+
+// the current CLI input to be tested
+type TestInput struct {
+	url        string
+	forceClone bool
+}
+
+type CliTests struct {
+	title                string
+	input                TestInput
+	exitCode             int
+	exactErrorMessages   map[string]bool // https://yourbasic.org/golang/implement-set/
+	errorMessageContains []string
+}
+
+/**
+ * Execute the CLI tests using our strategy
+ */
+func ExecuteCliTestsStrategy(t *testing.T, cliTests []CliTests) {
+	for _, tst := range cliTests {
+		t.Run(tst.title, func(t *testing.T) {
+			// https://github.com/smartystreets/goconvey/wiki#your-first-goconvey-test
+			// Only pass t into top-level Convey calls
+			Convey(tst.title, t, func() {
+
+				// Actual clone command to be tested
+				exitCode, errors := executeGitClone(tst.input.url, tst.input.forceClone)
+
+				Convey(fmt.Sprintf("The exit code should be %d", tst.exitCode), func() {
+					So(exitCode, ShouldEqual, tst.exitCode)
+				})
+
+				// Test exact error message cases
+				if len(tst.exactErrorMessages) > 0 {
+					Convey(fmt.Sprintf("With %d error message", len(tst.exactErrorMessages)), func() {
+						So(len(errors), ShouldEqual, len(tst.exactErrorMessages))
+						for _, errorMsg := range errors {
+							So(tst.exactErrorMessages[errorMsg.Error()], ShouldBeTrue)
+						}
+					})
+				}
+
+				// Test whether the error message contains strings
+				if len(tst.errorMessageContains) > 0 {
+					Convey(fmt.Sprintf("With %d error message", len(tst.errorMessageContains)), func() {
+						for _, errorMessageToken := range tst.errorMessageContains {
+							So(errors[0].Error(), ShouldContainSubstring, errorMessageToken)
+						}
+					})
+				}
+			})
+		})
 	}
 }
