dda59f877154431c67fa4477843f6ce65c3cd435
Marcello de Sales <marcello.desales@gmail.com>
2020-09-22 00:56:57 -0300

    :white_check_mark: Add PGP decrypt of secret to run tests
    The steps are described in the execution.

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -3,5 +3,4 @@
 # Decrypt the file
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/.ssh/id_cloner_test id_cloner_test.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="${ID_CLONER_TEST_PASSPHRASE}" --output $2 $1
